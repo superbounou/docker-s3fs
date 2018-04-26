ba147b7783d2a99d94c9e6aa03f679cac322c88b
Nicolas Bounoughaz <nicolas.bounoughaz@digitaslbi.fr>
2018-04-26 17:25:16 +0200

    first version

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker build -t superbounou/docker-s3fs:alpine .
+ACCESSID=$1
+ACCESSKEY=$2
+
+if [ "x$1" = "x" ]; then
+    echo "Please specify your S3 bucket credentials"
+    exit 1
+fi
+
+docker build --build-arg ACCESSID=${ACCESSID} --build-arg ACCESSKEY=${ACCESSKEY} -t docker-s3fs:alpine -f s3fs/Dockerfile .
---
+++ run.sh
@@ -0,0 +1,5 @@
+#:/bin/bash
+
+export S3_BUCKET_NAME=hermesdiane-develop
+
+docker-compose up
